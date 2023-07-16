ecd6f46e64245b247ecc5fec66eabe20273745fb
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-17 00:38:55 +0900

    feat: update header component contain logo

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,44 @@
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+
 import Navigation from './Navigation';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { PATH } from '../../constants';
+import { Title } from '../index';
 
 const Header = () => {
   return (
-    <>
-      Header
-      <Navigation />
-    </>
+    <SLayout>
+      <SContainer>
+        <Link to={PATH.HOME}>
+          <Logo height={24} />
+          <Title order={1} srOnly>
+            한국임상정보
+          </Title>
+        </Link>
+        <Navigation />
+      </SContainer>
+    </SLayout>
   );
 };
+
+const SLayout = styled.header`
+  width: 100%;
+  height: 56px;
+
+  background-color: ${props => props.theme.colors['bg-white']};
+`;
+
+const SContainer = styled.div`
+  max-width: 1040px;
+  height: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 0 auto;
+`;
 
 export default Header;
