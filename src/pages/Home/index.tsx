767d4c51324ee06e4f1c0b6dcf23b4b444c197a9
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-17 00:49:16 +0900

    feat: update Home page draft

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,62 @@
+import { styled } from 'styled-components';
+
+import { ReactComponent as CommunicatingPeople } from '../../assets/communicatingPeople.svg';
+import { ReactComponent as ResearchingPerson } from '../../assets/researchingPerson.svg';
+import { ReactComponent as DrawBoardPerson } from '../../assets/drawBoardPerson.svg';
+
+import Search from './Search';
+import { Title } from '../../components';
+
 const Home = () => {
-  return <>Home</>;
+  return (
+    <SLayout>
+      <SContainer>
+        <Title order={2} fz={34}>
+          국내 모든 임상시험 검색하고
+          <br />
+          온라인으로 참여하기
+        </Title>
+        <Search />
+        <SResearchingPerson />
+        <SCommunicatingPeople />
+        <SDrawBoardPerson />
+      </SContainer>
+    </SLayout>
+  );
 };
+
+const SLayout = styled.section`
+  background-color: ${props => props.theme.colors['bg-lightBlue']};
+`;
+
+const SContainer = styled.section`
+  max-width: 1000px;
+  position: relative;
+  padding: 80px 0px 160px;
+  margin: 0 auto;
+  text-align: center;
+  line-height: 1.6;
+`;
+
+const SResearchingPerson = styled(ResearchingPerson)`
+  width: 130px;
+  position: absolute;
+  right: 124px;
+  top: 280px;
+`;
+
+const SCommunicatingPeople = styled(CommunicatingPeople)`
+  width: 148px;
+  position: absolute;
+  left: 0;
+  top: 200px;
+`;
+
+const SDrawBoardPerson = styled(DrawBoardPerson)`
+  width: 116px;
+  position: absolute;
+  right: 20px;
+  top: 188px;
+`;
 
 export default Home;
