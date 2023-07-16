596a26c1a3b42297f6151501d23e1a10c5bfbd6c
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-17 00:43:36 +0900

    feat: add Navigation component relative to header component

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -1,5 +1,28 @@
+import { styled } from 'styled-components';
+
+import Button from '../Button';
+
+const DUMMY_NAVIGATORS = ['소개', '질문과 답변', '소식받기', '제휴/문의'];
+
 const Navigation = () => {
-  return <>Navigation</>;
+  return (
+    <nav>
+      <SContainer>
+        {DUMMY_NAVIGATORS.map(navigator => (
+          <li key={navigator}>
+            <Button>{navigator}</Button>
+          </li>
+        ))}
+      </SContainer>
+    </nav>
+  );
 };
+
+const SContainer = styled.ul`
+  display: flex;
+  gap: 20px;
+
+  list-style: none;
+`;
 
 export default Navigation;
