3634af849cc665c635ea4fd82bcb2b2048dde3aa
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-18 20:29:40 +0900

    refactor: improve cache util cohesion by using IIFE

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,17 +1,19 @@
-const expirationSeconds = 60 * 60 * 1000; // 60 * 60 * 1000 = 1hour
-const KEY = 'searchCache';
+export const cache = (() => {
+  const EXPIRATION_TIME = 60 * 60 * 1000; // 60 * 60 * 1000 = 1hour
+  const SEARCH_KEY = 'searchCache';
 
-export const cache = {
-  async set(url: string, data: unknown) {
-    const cacheStorage = await caches.open(KEY);
-    const response = new Response(JSON.stringify({ data, expirationTime: Date.now() + expirationSeconds * 1000 }));
+  return {
+    async set(url: string, data: unknown) {
+      const cacheStorage = await caches.open(SEARCH_KEY);
+      const response = new Response(JSON.stringify({ data, expirationTime: Date.now() + EXPIRATION_TIME }));
 
-    await cacheStorage.put(url, response);
-  },
-  async get(url: string) {
-    const cacheStorage = await caches.open(KEY);
-    const cacheResponse = await cacheStorage.match(url);
+      await cacheStorage.put(url, response);
+    },
+    async get(url: string) {
+      const cacheStorage = await caches.open(SEARCH_KEY);
+      const cacheResponse = await cacheStorage.match(url);
 
-    return cacheResponse;
-  },
-};
+      return cacheResponse;
+    },
+  };
+})();
